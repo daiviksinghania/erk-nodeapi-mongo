4dace9cc23afb71ae27fe0ad5ac0e56605c93d16
errahulkumar <er.rahulkumar13@gmail.com>
2021-08-06 10:49:18 +0530

    node api

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,9 @@ import { connect, connection } from 'mongoose';
 const mongoConnectionUrl = 'mongodb://localhost:27017/students'
 
 try {
-    await mongoose.connect(mongoConnectionUrl, { useNewUrlParser: true });
+    await mongoose.connect(mongoConnectionUrl, { useNewUrlParser: true, useUnifiedTopology: true});
+    console.log("database connection Established");
 } catch (error) {
+    console.log(error);
     handleError(error);
 }
--- src/controllers/student.js
+++ src/controllers/student.js
@@ -6,16 +6,18 @@ import { JWT_SECRET } from "../config/constants";
 
 export const addNewStudent = async(req, res) => {
     console.log("reached at controller");
+    let encpwd = hashSync(req.body.student.pwd, 10);
+    console.log(encpwd)
     let student = new Student({
         name: req.body.student.name,
         email: req.body.student.email,
-        passhash: hashSync(req.body.student
-            .pwd, 10),
+        passhash: encpwd,
         created:(new Date()).toLocaleDateString()
     })
     console.log(student)
     try {
-        const newStudent = await student.save()
+        await student.save()
+        const newStudent = student;
         res.status(201).json(newStudent)
     } catch (err) {
         res.status(400).json({ message: err.message })
